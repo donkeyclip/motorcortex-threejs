ffdc4a37eaa7f5cd5c58c472542c7e1a56c330f0
Theofilos Papadopoulos <theofilos.p@kissmybutton.gr>
2018-07-04 13:40:33 +0300

    style: progress
    progress

--- webpack.config.js
+++ webpack.config.js
@@ -1,76 +1,78 @@
-const path = require( 'path' );
-const webpack = require( 'webpack' );
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
+  context: path.resolve(__dirname),
 
-    context: path.resolve( __dirname ),
+  entry: "./index.js",
 
-    entry: 'index.js',
+  resolve: {
+    extensions: [".js"],
+    modules: [path.resolve("./"), "node_modules"]
+  },
+  output: {
+    filename: "bundle.js",
+    // the output bundle
 
-    resolve: {
-        extensions: [
-            '.js'
-        ],
-        modules: [
-            path.resolve( './' ),
-            'node_modules'
+    path: path.resolve(__dirname, "./dist")
+  },
+
+  devtool: "cheap-module-eval-source-map",
+  mode: "development",
+
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        use: ["babel-loader"],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader",
+            options: { sourceMap: true } // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader",
+            options: { sourceMap: true } // translates CSS into CommonJS
+          },
+          {
+            loader: "sass-loader",
+            options: { sourceMap: true } // compiles Sass to CSS
+          }
         ]
-    },
-    output: {
-        filename: 'bundle.js',
-        // the output bundle
+      }
+    ]
+  },
 
-        path: path.resolve( __dirname, './dist' )
+  plugins: [
+    new webpack.ProvidePlugin({
+      Promise: "es6-promise",
+      fetch:
+        "imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch"
+    }),
 
-    },
+    new webpack.HotModuleReplacementPlugin(),
+    // enable HMR globally
 
-    devtool: 'cheap-module-eval-source-map',
-    mode: 'development',
+    new webpack.NamedModulesPlugin(),
+    // prints more readable module names in the browser console on HMR updates
 
-    module: {
-        rules: [
-            {
-                test: /\.js?$/,
-                use: [ 'babel-loader' ],
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css$/,
-                use: [ 'style-loader', 'css-loader' ]
-            },
-            {
-                test: /\.scss$/,
-                use: [ {
-                    loader: "style-loader", options: { sourceMap: true } // creates style nodes from JS strings
-                }, {
-                    loader: "css-loader", options: { sourceMap: true } // translates CSS into CommonJS
-                }, {
-                    loader: "sass-loader", options: { sourceMap: true } // compiles Sass to CSS
-                } ]
-            }
-        ]
-    },
+    new webpack.NoEmitOnErrorsPlugin()
+    // do not emit compiled assets that include errors
+  ],
 
-    plugins: [
-
-        new webpack.ProvidePlugin( { 'Promise': 'es6-promise', 'fetch': 'imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch' } ),
-
-        new webpack.HotModuleReplacementPlugin( ),
-        // enable HMR globally
-
-        new webpack.NamedModulesPlugin( ),
-        // prints more readable module names in the browser console on HMR updates
-
-        new webpack.NoEmitOnErrorsPlugin( ),
-        // do not emit compiled assets that include errors
-
-    ],
-
-    devServer: {
-        watchContentBase: true, // initiate a page refresh if static content changes
-        host: '0.0.0.0',
-        port: 8080,
-        historyApiFallback: false,
-        hot: false
-    }
+  devServer: {
+    watchContentBase: true, // initiate a page refresh if static content changes
+    host: "0.0.0.0",
+    port: 8080,
+    historyApiFallback: false,
+    hot: false
+  }
 };
